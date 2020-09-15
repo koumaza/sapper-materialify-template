f823f468f54431f671d695b93af86182e5cdd9be
TheComputerM <mudit.harshit99@outlook.com>
2020-09-15 23:17:04 +0530

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Button } from 'svelte-materialify/src/'
+</script>
+
+<svelte:head>
+	<title>Svelte Materialify Sapper template</title>
+</svelte:head>
+
+<Button>
+	Test
+</Button>
